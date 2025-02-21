38a370b9f6751c621c435e0cfd07e1dfe0c5c723
Huangzzz <huangz2350@163.com>
2025-02-21 17:17:56 +0800

    训练推理模型 (Reasoning Model) add commit over

--- train_distill_reason.py
+++ train_distill_reason.py
@@ -20,7 +20,17 @@ from model.LMConfig import LMConfig
 from model.dataset import SFTDataset
 
 warnings.filterwarnings('ignore')
+'''
+DeepSeek-R1实在太火了，几乎重新指明了未来LLM的新范式。 
+论文指出>3B的模型经历多次反复的冷启动和RL奖励训练才能获得肉眼可见的推理能力提升。 
+最快最稳妥最经济的做法，以及最近爆发的各种各样所谓的推理模型几乎都是直接面向数据进行蒸馏训练， 但由于缺乏技术含量，蒸馏派被RL派瞧不起（hhhh）。 
+本人迅速已经在Qwen系列1.5B小模型上进行了尝试，很快复现了Zero过程的数学推理能力。 
+然而一个遗憾的共识是：参数太小的模型直接通过冷启动SFT+GRPO几乎不可能获得任何推理效果。 
+MiniMind2第一时间只能坚定不移的选择做蒸馏派，日后基于0.1B模型的RL如果同样取得小小进展会更新此部分的训练方案。
 
+上述描述也是该方案验证的局限性，因为原模型<3B，所以无法直接进行RLHF训练，只能通过蒸馏的方式进行训练。
+如果>3B 模型训练，可以参考unsloth的GRPO方案进行验证
+'''
 
 def Logger(content):
     if not ddp or dist.get_rank() == 0:
@@ -37,6 +47,7 @@ def train_epoch(epoch, wandb):
     end_of_think_ids = tokenizer('</think>').input_ids
     start_of_answer_ids = tokenizer('<answer>').input_ids
     end_of_answer_ids = tokenizer('</answer>').input_ids
+    # 交叉熵损失函数
     loss_fct = nn.CrossEntropyLoss(reduction='none')
     start_time = time.time()
     for step, (X, Y, loss_mask) in enumerate(train_loader):
@@ -53,6 +64,14 @@ def train_epoch(epoch, wandb):
                 res.logits.view(-1, res.logits.size(-1)),
                 Y.view(-1)
             ).view(Y.size())
+            
+            '''
+            torch.isin 函数检查 Y 中的每个元素是否存在于合并后的 ID 张量中。
+            torch.isin 函数的作用类似于 Python 中的 in 操作符，但它是针对张量操作的。
+            该函数返回一个布尔张量，其中每个元素表示 Y 中对应位置的元素是否存在于测试元素张量中。
+            通过这种方式，代码可以有效地筛选出 Y 中属于特定 ID 列表的元素，并将结果存储在布尔张量 sp_ids 中。
+            这个布尔张量可以在后续的计算中用作掩码，帮助模型关注特定的元素或位置，从而实现更精细的控制和优化。
+            '''
             sp_ids = torch.isin(Y.view(-1),
                                 torch.tensor(start_of_think_ids + end_of_think_ids
                                              + start_of_answer_ids + end_of_answer_ids
@@ -60,6 +79,8 @@ def train_epoch(epoch, wandb):
             # 在 sp_ids 对应的位置增加额外的惩罚
             loss_mask = loss_mask.view(-1)
             loss_mask_sum = loss_mask.sum()
+            # 蒸馏过程虽然和SFT一样，但实验结果是模型难以每次都符合模板规范的回复，即脱离思考和回复标签约束。 
+            # 这里的小技巧是增加标记位置token的损失惩罚，
             loss_mask[sp_ids] = 10
             loss_mask = loss_mask.view(Y.size())
             loss = (loss * loss_mask).sum() / loss_mask_sum
@@ -112,7 +133,9 @@ def init_model(lm_config):
     tokenizer = AutoTokenizer.from_pretrained('./model/minimind_tokenizer')
     model = MiniMindLM(lm_config)
     moe_path = '_moe' if lm_config.use_moe else ''
-    ckp = f'./out/rlhf_{lm_config.dim}{moe_path}.pth'
+    # 读取预训练模型:因为我显存原因，rlhf没有正常训练完成，选择用full_sft模型代替
+    ckp = f'./out/full_sft_{lm_config.dim}{moe_path}.pth'  
+    # ckp = f'./out/rlhf_{lm_config.dim}{moe_path}.pth'
     state_dict = torch.load(ckp, map_location=args.device)
     model.load_state_dict(state_dict, strict=False)
     Logger(f'LLM总参数量：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f} 百万')
